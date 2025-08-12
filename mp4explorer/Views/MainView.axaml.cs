88c53861bf7e671f5e7a24f824478fa77cb3a25a
sandreas <2050604+sandreas@users.noreply.github.com>
2025-08-12 09:23:59 +0200

    initial commit

---
+++ mp4explorer/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace mp4explorer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ mp4explorer/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace mp4explorer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
